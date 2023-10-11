d262cddb838f4035e644405138010fa56f8f541e
Abner Jimenez <71862339+abnerjg@users.noreply.github.com>
2023-10-11 17:09:38 -0600

    Update list-view-router.js

--- list-view-router.js
+++ list-view-router.js
@@ -1,23 +1,18 @@
+// Importa las dependencias necesarias, incluyendo Express
 const express = require('express');
-const listViewRouter = express.Router();
+const router = express.Router();
 
-listEditRouter.post('/create', (req, res) => {
-    const { description, completed } = req.body;
-    const newTask = {
-      id: tasks.length + 1,
-      description,
-      completed,
-    };
-    tasks.push(newTask);
-    res.json(newTask);
-  });
-
-  // Middleware para gestionar la validez de los parámetros
-router.use('/:parametro', (req, res, next) => {
-  const parametro = req.params.parametro;
+// Middleware para gestionar parámetros correctos
+router.use((req, res, next) => {
+  const parametro = req.params.parametro; // Asegúrate de reemplazar 'parametro' con el nombre de tu parámetro
   if (parametro && parametro.length > 0) {
+    // Si el parámetro es válido, pasa al siguiente middleware
     next();
   } else {
+    // Si el parámetro no es válido, responde con un código 400 (Solicitud incorrecta)
     res.status(400).send('Parámetro incorrecto');
   }
 });
+
+// Define tus rutas y controladores aquí
+module.exports = router;
